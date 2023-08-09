05ff84b5cab6975866fad4b31030ee7309d7868a
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-08-09 05:09:02 -0400

    feat: add cool line wrap extmark feature

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -105,3 +105,24 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
     end,
 
 })
+
+-- Display '󰘍' before lines that are wrapped by a `\` character
+-- Pretty cool feature
+vim.api.nvim_create_augroup("Line Break Extmarks", {})
+vim.api.nvim_create_autocmd({ "InsertLeave", "BufEnter" }, {
+    pattern = { '*.py' }, --  Shell filetypes?
+    callback = function(opts)
+        for line = 0, vim.api.nvim_buf_line_count(opts.buf)-1, 1 do
+            local ns_id = vim.api.nvim_create_namespace("Line Break Extmarks")
+            if vim.api.nvim_buf_get_lines(opts.buf, line, line + 1, true)[1]:sub(-1) == '\\' then
+                vim.api.nvim_buf_set_extmark(opts.buf, ns_id, line + 1, vim.fn.indent(line), {
+                    virt_text = { { '󰘍', 'Normal' } },
+                    virt_text_win_col = vim.fn.indent(line) + 2,
+                    id = line,
+                })
+            else
+                vim.api.nvim_buf_del_extmark(opts.buf, ns_id, line)
+            end
+        end
+    end
+})
