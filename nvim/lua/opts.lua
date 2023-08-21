3f174e60912916508e51488506baf677f076dbb3
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-08-21 10:13:05 +0200

    style
    fixpu

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -1,3 +1,4 @@
+local o = vim.opt
 function _G.custom_fold_text()
     local line = vim.fn.getline(vim.v.foldstart)
     line = string.gsub(line, '{{{', "") --}}}
@@ -19,67 +20,62 @@ function _G.custom_fold_text()
     return line .. ' '
 end
 
-vim.opt.foldtext = 'v:lua.custom_fold_text()'
-local opts = {
-    fillchars = {
-        stl = ' ',
-        stlnc = ' ',
-        foldopen = '',
-        foldclose = '',
-        fold = ' ',
-        foldsep = ' ',
-        diff = '╱',
-        eob = ' ',
+o.foldtext = 'v:lua.custom_fold_text()'
+o.fillchars = {
+    stl = ' ',
+    stlnc = ' ',
+    foldopen = '',
+    foldclose = '',
+    fold = ' ',
+    foldsep = ' ',
+    diff = '╱',
+    eob = ' ',
 
-        -- -- horiz = ' ',
-        -- horizup = '─',
-        -- horizdown = '─',
-        -- -- horizup = ' ',
-        -- -- horizdown = ' ',
-        -- vert = ' ',
-        -- vertleft = ' ',
-        -- vertright = ' ',
-        -- verthoriz = ' ',
+    -- -- horiz = ' ',
+    -- horizup = '─',
+    -- horizdown = '─',
+    -- -- horizup = ' ',
+    -- -- horizdown = ' ',
+    -- vert = ' ',
+    -- vertleft = ' ',
+    -- vertright = ' ',
+    -- verthoriz = ' ',
 
-    },
-    foldtext = 'v:lua.custom_fold_text()',
-    listchars = {
-        tab = '▸ ',
-    },
-    list = true,
-    foldcolumn = '2',
-    signcolumn = 'yes:1',
-    number = true,
-    relativenumber = true,
-    breakindent = true,
-    mouse = 'a',
-    hidden = true,
-    autoindent = true,
-    wildmenu = true,
-    smartcase = true,
-    ignorecase = true,
-    termguicolors = true,
-    tabstop = 4,
-    shiftwidth = 4,
-    expandtab = true,
-    splitbelow = true,
-    splitright = true,
-    laststatus = 3,
-    scrolloff = 3,
-    incsearch = true,
-    wrap = false,
-    showmode = false,
-    cursorline = true,
-    foldmethod = 'marker',
-    background = 'dark',
-    virtualedit = 'block',
-    matchpairs = '(:),{:},[:],<:>',
-    undofile = true,
-    -- smoothscroll = true,
 }
-for k, v in pairs(opts) do
-    vim.opt[k] = v
-end
+o.foldtext = 'v:lua.custom_fold_text()'
+o.listchars = {
+    tab = '▸ ▸',
+}
+o.list = true
+o.foldcolumn = '2'
+o.signcolumn = 'yes:1'
+o.number = true
+o.relativenumber = true
+o.breakindent = true
+o.mouse = 'a'
+o.hidden = true
+o.autoindent = true
+o.wildmenu = true
+o.smartcase = true
+o.ignorecase = true
+o.termguicolors = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.splitbelow = true
+o.splitright = true
+o.laststatus = 3
+o.scrolloff = 3
+o.incsearch = true
+o.wrap = false
+o.showmode = false
+o.cursorline = true
+o.foldmethod = 'marker'
+o.background = 'dark'
+o.virtualedit = 'block'
+o.matchpairs = '(:),{:},[:],<:>'
+o.undofile = true
+-- o.smoothscroll = true
 
 vim.g.bufferline_show_all = false
 vim.g.lualine_verbose = false
