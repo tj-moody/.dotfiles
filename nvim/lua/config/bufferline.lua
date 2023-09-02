a93c8027549a32a12da56eb51698cb30f4fe3275
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-09-01 21:25:27 -0400

    cleanup

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -28,41 +28,14 @@ require("bufferline").setup {
             return false
         end,
         mode = "buffers",
-        -- numbers = "none",
         numbers = function()
             return ''
         end,
-        close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-        middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
         indicator = {
             icon = '▎',                    -- this should be omitted if indicator style is not 'icon'
             style = 'icon',
-            -- style = 'underline',
         },
-        buffer_close_icon = '',
-        modified_icon = '●',
-        close_icon = '',
-        left_trunc_marker = '',
-        right_trunc_marker = '',
-        --- name_formatter can be used to change the buffer's label in the bufferline.
-        --- Please note some names can/will break the
-        --- bufferline so use this at your discretion knowing that it has
-        --- some limitations that will *NOT* be fixed.
-        max_name_length = 18,
-        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-        truncate_names = true,  -- whether or not tab names should be truncated
-        tab_size = 18,
         diagnostics = "nvim_lsp",
-        diagnostics_update_in_insert = false,
-        -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
-            -- return "("..count..")"
-            -- return "" .. count
-            return ""
-        end,
-        -- NOTE: this will be called a lot so don't do any heavy processing here
         offsets = {
             {
                 filetype = "NvimTree",
@@ -71,26 +44,7 @@ require("bufferline").setup {
                 separator = ' ',
             },
         },
-        color_icons = true,
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        -- show_buffer_default_icon = true, -- deprecated
-        show_close_icon = false,
-        show_tab_indicators = true,
-        show_duplicate_prefix = false,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        -- can also be a table containing 2 custom separators
-        -- [focused and unfocused]. eg: { '|', '|' }
-        -- separator_style = "thick",
         separator_style = { ' ', ' ' },
-        enforce_regular_tabs = false,
-        always_show_bufferline = true,
-        hover = {
-            enabled = true,
-            delay = 200,
-            reveal = { 'close' },
-        },
-        sort_by = 'insert_after_current',
         custom_areas = {
             left = function()
                 return { { text = ' ', bg = '', fg = '', } }
