1391549a9d9317fdbb02176ad21ef66fd1dd2856
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-09-02 23:50:57 -0400

    feat: display theme tui inline, toggle live updating theme, formatting

--- bash/theme.bash
+++ bash/theme.bash
@@ -5,38 +5,38 @@ if [[ $# -ne 1 ]]; then
 fi
 
 tput civis
-printf '\e[?1049h'
 
 declare -a themestrings=(
-"\e[0;34m noclownfiesta  \e[0m"
-"\e[1;36m kanagawa       \e[0m"
-"\e[1;34m kanagawa_dark  \e[0m"
-"\e[0;33m gruvbox        \e[0m"
-"\e[1;31m marsbox        \e[0m"
-"\e[0;34m tokyonight     \e[0m"
-"\e[0;32m oxocarbon      \e[0m"
-"\e[0;35m catppuccin     \e[0m"
-"\e[0;32m everforest     \e[0m"
-"\e[0;33m ayu            \e[0m"
-"\e[0;36m midnightclub   \e[0m"
+    "\e[0;34mnoclownfiesta  \e[0m"
+    "\e[1;36mkanagawa       \e[0m"
+    "\e[1;34mkanagawa_dark  \e[0m"
+    "\e[0;33mgruvbox        \e[0m"
+    "\e[1;31mmarsbox        \e[0m"
+    "\e[0;34mtokyonight     \e[0m"
+    "\e[0;32moxocarbon      \e[0m"
+    "\e[0;35mcatppuccin     \e[0m"
+    "\e[0;32meverforest     \e[0m"
+    "\e[0;33mayu            \e[0m"
+    "\e[0;36mmidnightclub   \e[0m"
 )
 
 declare -a themeslist=(
-"noclownfiesta"
-"kanagawa"
-"kanagawa_dark"
-"gruvbox"
-"marsbox"
-"tokyonight"
-"oxocarbon"
-"catppuccin"
-"everforest"
-"ayu"
-"midnightclub"
+    "noclownfiesta"
+    "kanagawa"
+    "kanagawa_dark"
+    "gruvbox"
+    "marsbox"
+    "tokyonight"
+    "oxocarbon"
+    "catppuccin"
+    "everforest"
+    "ayu"
+    "midnightclub"
 )
 
 MAX_INDEX=$( expr ${#themestrings[@]} - 1 )
 THEME=$1
+LIVE_UPDATE=false
 
 index=4 # marsbox
 for i in "${!themeslist[@]}"; do
@@ -44,15 +44,6 @@ for i in "${!themeslist[@]}"; do
         index=$i
     fi
 done
-
-cleard() {
-    clear
-    for (( i=0; i<$LINES; i++)); do
-        # echo -ne "\n"
-        printf "\n"
-    done
-}
-cleard
 
 index_to_theme() {
     echo "${themeslist[$1]}";
@@ -68,23 +59,25 @@ print_themes() {
         fi
         echo -e "${themestrings[$i]}"
     done
-    echo -e "\033]50;SetProfile=$(index_to_theme $1)\a"
+    if [[ "$LIVE_UPDATE" == "true" ]]; then
+        echo -ne "\033]50;SetProfile=$(index_to_theme $1)\a"
+    fi
 }
 
 exit_loop() {
-    echo -e "\033]50;SetProfile=$THEME\a"
+    echo -ne "\033]50;SetProfile=$THEME\a"
     if [[ "$TERM" == "xterm-kitty" ]]; then
         kitty +kitten themes --reload-in=all $THEME
     fi
 
+    echo -e "${themestrings[$index]}"
     tput cnorm
-    printf '\e[?1049l'
 }
 
 while : ; do
     print_themes $index
     echo -ne "\e[${MAX_INDEX}A"
-    echo -ne "\e[2A"
+    echo -ne "\e[1A"
     read -s -p "" -n 1 key
     case $key in
         'j')
@@ -107,6 +100,14 @@ while : ; do
 
             exit_loop
             break
+            ;;
+        'u')
+            if [[ "$LIVE_UPDATE" == "true" ]]; then
+                LIVE_UPDATE=false
+                echo -ne "\033]50;SetProfile=${THEME}\a"
+            else
+                LIVE_UPDATE=true
+            fi
             ;;
         'q')
             exit_loop
