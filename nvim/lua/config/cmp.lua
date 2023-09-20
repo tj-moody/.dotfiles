df666a62ef178b7f062b8ebc9b01ecdaa0c8f982
tj-moody <92702993+tj-moody@users.noreply.github.com>
2023-09-20 16:45:24 -0400

    change c-e to match fish shell

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -56,7 +56,7 @@ cmp.setup({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         -- ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-e>'] = cmp.mapping.confirm({ select = false }),
         -- Accept currently selected item. Set `select`
         -- to `false` to only confirm explicitly selected items.
         ['<CR>'] = cmp.mapping.confirm({ select = false }),
