d416999ed043ec764d1ccf9be45fcda59cb1ccc4
tj-moody <92702993+tj-moody@users.noreply.github.com>
2025-05-21 16:54:22 -0400

    clean up nvim launcher

--- bash/nv.bash
+++ bash/nv.bash
@@ -2,12 +2,7 @@
 
 first="true"
 while true; do
-    if [[ "$first" == "true" ]]; then
-        first="false"
-        /Users/tj/.local/share/bob/v0.11.1/bin/nvim "$@"
-    else
-        /Users/tj/.local/share/bob/v0.11.1/bin/nvim "$@"
-    fi
+    nvim "$@"
     status="$?"
     if [ "$status" == "3" ]; then
         bash ~/.dotfiles/bash/theme.bash arg
--- bash/nvr.bash
+++
@@ -1,8 +0,0 @@
-#!/opt/homebrew/bin/bash
-
-while true; do
-    /Users/tj/.local/share/bob/v0.11.1/bin/nvim --cmd "let g:tj_reloaded = 'true'" "$@"
-    if [ $? -ne 1 ]; then
-        break
-    fi
-done
