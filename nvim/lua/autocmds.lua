ff627227ed4e700baff790d70029aabe9747d094
tj-moody <92702993+tj-moody@users.noreply.github.com>
2025-12-01 09:04:07 -0500

    refactor: remove old aesthetic extmark autocmds, clean up session save/restore autocmds, open nvimtree when opening directory properly

---
+++ nvim/lua/autocmds.lua
@@ -0,0 +1,72 @@
+---- Strip trailing spaces before write
+-- https://github.com/2KAbhishek/nvim2k/blob/main/lua/nvim2k/autocmd.lua
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*" },
+    group = vim.api.nvim_create_augroup("Format On Save", {}),
+    callback = function(_)
+        local save = vim.fn.winsaveview()
+        vim.cmd([[ %s/\s\+$//e ]])
+        vim.fn.winrestview(save) ---@diagnostic disable-line
+    end,
+})
+
+---- Auto-restore session
+vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+    pattern = { "*" },
+    group = vim.api.nvim_create_augroup("Auto-Session", {}),
+    callback = function(_)
+        if vim.g.in_pager_mode then
+            return
+        end
+
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            local name = vim.fn.bufname(buf)
+            local ft = vim.bo[buf].filetype
+
+            if ft == "qf" or ft == "NvimTree" or ft == "projterm" or name:sub(1, 7) == "term://" then
+                vim.cmd("silent! bd! " .. buf)
+            end
+        end
+
+        if vim.g.no_save == nil or vim.g.no_save == false then
+            vim.cmd("AutoSession save")
+        end
+    end,
+})
+
+-- Auto-load nvim-tree when opening a directory on startup
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        local arg = vim.fn.argv(0)
+        if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+            vim.cmd("silent! %bd")
+            require("nvim-tree.api").tree.open()
+            vim.cmd.only()
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = { "*" },
+    callback = function()
+        if vim.fn.expand("%:p"):sub(1, 7) == "term://" then ---@diagnostic disable-line
+            vim.cmd("setlocal nonumber norelativenumber nobuflisted")
+            local opts = { noremap = true, silent = true, buffer = 0 }
+            vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+            vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+            vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+            vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+            vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+            vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+            vim.cmd("norm! i")
+        end
+    end,
+})
+
+-- <CR> to run cmdwin entry
+vim.api.nvim_create_autocmd("CmdWinEnter", {
+    pattern = { "*" },
+    callback = function(opts)
+        vim.keymap.set("n", "<CR>", "<C-C><CR>", { noremap = true, silent = true, buffer = opts.buf })
+    end,
+})
