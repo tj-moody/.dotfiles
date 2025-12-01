fd03e8aa1fceebfb61b6a335f98047ec76ff1a31
tj-moody <92702993+tj-moody@users.noreply.github.com>
2025-12-01 08:40:15 -0500

    fix: restart nv properly after passing arguments

--- bash/nv.bash
+++ bash/nv.bash
@@ -3,6 +3,7 @@
 while true; do
     /Users/tj/.local/share/bob/v0.11.1/bin/nvim "$@"
     status="$?"
+    set --
     if [ "$status" == "3" ]; then
         ~/.dotfiles/scripts/theme
     elif [ "$status" != "5" ]; then
