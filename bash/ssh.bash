df1e10a21680628f18e9b85e50b7f7f5c1f6d549
tj-moody <92702993+tj-moody@users.noreply.github.com>
2026-01-31 20:10:19 -0500

    feat: show ssh connection in tmux window status

--- bash/ssh.bash
+++ bash/ssh.bash
@@ -3,11 +3,15 @@
 function ssh_cleanup {
     if [[ -z "$TMUX" ]]; then
         printf '\033]104'
+    else
+        tmux set -p @ssh_host ""
     fi
 }
 
 if [[ -z "$TMUX" ]]; then
     printf "\x1b]11;#0a0c21\x1b\\"
+else
+    tmux set -p @ssh_host "$1"
 fi
 
 trap ssh_cleanup SIGINT SIGTERM EXIT
