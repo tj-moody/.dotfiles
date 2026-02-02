dcf3e595c281b5b77247c93e2fc36eba2e8394e2
tj-moody <92702993+tj-moody@users.noreply.github.com>
2026-02-02 12:26:54 -0500

    migrate treesitter TODO: don't install everything on start

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,88 +1,77 @@
 local M = {}
 local function setup_treesitter()
+    local ts = safe_require("nvim-treesitter")
     ---@diagnostic disable missing-fields
-    safe_require("nvim-treesitter.parsers").get_parser_configs().asm = {
-        install_info = {
-            url = "https://github.com/rush-rs/tree-sitter-asm.git",
-            files = { "src/parser.c" },
-            branch = "main",
-        },
-    }
+    -- safe_require("nvim-treesitter.parsers").get_parser_configs().asm = {
+    --     install_info = {
+    --         url = "https://github.com/rush-rs/tree-sitter-asm.git",
+    --         files = { "src/parser.c" },
+    --         branch = "main",
+    --     },
+    -- }
 
-    safe_require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-            "c",
-            "lua",
-            "vim",
-            -- "help",
-            "rust",
-            "javascript",
-            "typescript",
-            "python",
-            "bash",
-            "go",
-            "html",
-            "css",
-            "java",
-            "markdown",
-            "norg",
-            "asm",
-        },
-
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-            enable = true,
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-
-            additional_vim_regex_highlighting = true,
-        },
-        indent = {
-            enable = true,
-        },
-        autotag = {
-            enable = true,
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                },
-                include_surrounding_whitespace = true,
-            },
-        },
+    ts.install({
+        "c",
+        "lua",
+        "vim",
+        -- "help",
+        "rust",
+        "javascript",
+        "typescript",
+        "python",
+        "bash",
+        "go",
+        "html",
+        "css",
+        "java",
+        "markdown",
+        "asm",
+    }, {
+        summary = false,
     })
 
     vim.g.skip_ts_context_commentstring_module = true
 
     vim.api.nvim_set_hl(0, "TreesitterContext", { link = "Normal" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "*" },
+        callback = function()
+            local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+
+            if not parser_installed then
+                require("nvim-treesitter").install({ parser_name }):wait(30000)
+            end
+
+            parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+
+            if parser_installed then
+                vim.treesitter.start(bufnr, parser_name)
+                vim.bo.syntax = 'on'
+                -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                -- vim.wo.foldmethod = 'expr'
+            end
+        end,
+    })
 end
 
 M.spec = {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         event = "LazyFile",
         dependencies = {
             { "windwp/nvim-ts-autotag" },
-            { "JoosepAlviste/nvim-ts-context-commentstring", config = true },
             { "nvim-treesitter/nvim-treesitter-context", config = true },
-            { "windwp/nvim-ts-autotag" },
-            { "nvim-treesitter/nvim-treesitter-textobjects" },
+            { "JoosepAlviste/nvim-ts-context-commentstring", config = true },
+            -- { "nvim-treesitter/nvim-treesitter-textobjects" },
+            -- { "nvim-treesitter/playground" },
             {
                 "Wansmer/treesj",
                 config = { use_default_keymaps = false },
                 keys = { { "<c-s>", "<CMD>TSJToggle<CR>", desc = "Split/Join" } },
             },
-            { "nvim-treesitter/playground" },
             { "rush-rs/tree-sitter-asm" },
         },
         config = setup_treesitter,
